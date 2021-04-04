5a08290df3e265e118ad18a649f29b941ba960e0
Kristian Daskalov <81424747+Uiyx@users.noreply.github.com>
2021-04-04 23:21:44 +0300

    Update config.mk

--- config.mk
+++ config.mk
@@ -7,8 +7,13 @@ VERSION = 6.2
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+
+
+X11INC = usr/include/X11 #For Void linux
+X11LIB = usr/lib/X11 # For Void linux
+
+# X11INC = /usr/X11R6/include # For other distributions
+# X11LIB = /usr/X11R6/lib # For other linux distributions
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
